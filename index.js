adfbc90ff27a3624c54f6a45f00716342463cca6
SAMU MODS FF <bsbsqange@gmail.com>
2026-07-28 04:03:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-cons express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
 app.get('/', (req, res) => {
-  res.send('¡Tu servidor está funcionando!');
+    res.send('✅ Servidor funcionando correctamente!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
